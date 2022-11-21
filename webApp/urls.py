d572c1f9143bc2b88d23fe4a2067839d139d93ea
Carl0sss <rafailan99@gmail.com>
2022-11-20 20:23:08 -0600

    add administrator login

--- webApp/urls.py
+++ webApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.urls import include
 
 # enlaces para mostrar las vistas (URLS)
 # path(para pasar parametros, llamado a las  views, nombre de la url)
