bbecbc14fb1453a4bf6938c2bf219b4013eead27
Carl0sss <rafailan99@gmail.com>
2022-11-25 22:41:42 -0600

    .\internacionales\

--- internacionales/urls.py
+++ internacionales/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('verNoticiasLibertadores/<id>/', views.verNoticiasLibertadores, name='id'),
     path('verEquiposLibertadores', views.verEquiposLibertadores),
     path('verTodasNoticiasLibertadores', views.verTodasNoticiasLibertadores, name='liber'),
-
+    path('verNoticiasChampions/<id>/', views.verNoticiasChampions, name='notiChampions'),
+    path('verEquiposChampions', views.verEquiposChampions, name="equipoChampions"),
+    path('verTodasNoticiasChampions', views.verTodasNoticiasChampions, name='todasChampions'),
+    path('verPerfilEquipoChampions/<id>/', views.verPerfilEquipoChampions, name = 'perfilChampions'),
 ]
